8a920e3293fb3f97714f6bef98f4a3999c218407
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-09-04 19:33:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./Components/Header.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
+  const links = [
+    { label: "HOME", path: "/home" },
+    { label: "TIMELINE", path: "/timeline" },
+    { label: "DOMAINS", path: "/domains" },
+    { label: "RULES", path: "/rules" },
+    { label: "PRIZE", path: "/prize" },
+    { label: "SPONSORS", path: "/sponsors" },
+    { label: "CONTACT", path: "/contact" },
+  ];
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <header className="flex justify-center gap-10 border-dashed border-2 border-sky-500 py-5  m-5 font-bold w">
+        {links.map((item, index) => (
+          <a href="item.path" key={index}>
+            {item.label}
+          </a>
+        ))}
+      </header>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
